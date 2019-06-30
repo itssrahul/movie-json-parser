ee6a86a5f2d23865379067911cdb2812fb80e867
Rahul Soni <therimpact@gmail.com>
2019-06-30 19:45:43 +0530

    Some Optimizations

--- src/main/java/com/forster/Main.java
+++ src/main/java/com/forster/Main.java
@@ -1,94 +1,73 @@
 package com.forster;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonElement;
 import com.google.gson.reflect.TypeToken;
-import com.google.gson.stream.JsonWriter;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.*;
 import java.util.*;
 import java.util.stream.Collectors;
+
+import static java.lang.System.out;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         Main mainObj = new Main();
 
-        System.out.println("loading input json...");
+        // loading input json...
+        out.println("loading input json...");
         File inputJsonFile = mainObj.loadInputFile();
-        System.out.println("loaded input json...");
+        out.println("loaded input json...");
 
-        System.out.println("Extracting movies from input json...");
+        // Extracting movies from input json...
+        out.println("Extracting movies from input json...");
         List<Movies> moviesList = mainObj.extractMoviesFromJson(inputJsonFile);
-        System.out.println("Extracted movies from input json...; moviesCount : "+moviesList.size());
+        out.println("Extracted movies from input json...; moviesCount : "+moviesList.size());
 
-        System.out.println("Grouping movies by FSK Categories..");
+        // Grouping movies by FSK Categories..
+        out.println("Grouping movies by FSK Categories..");
         Map<String, ArrayList<Movie>> movieMap = mainObj.groupMoviesByCountAndFSKType(moviesList);
-        System.out.println("Grouped movies by FSK Categories..total categories: "+ movieMap.size());
-        System.out.println("All movies categories are : "+ movieMap.entrySet().toString());
+        out.println("Grouped movies by FSK Categories..total categories: "+ movieMap.size());
+        out.println("All movies categories are : "+ movieMap.entrySet().toString());
 
-        System.out.println("Writing output Json results...");
-        //mainObj.createMoviesOutputJson(movieMap);
-        List<FskCategory> fskCategoryList = mainObj.createCategorisedMoviesList(movieMap);
-        mainObj.writeJsonToFile(fskCategoryList);
-        System.out.println("Successfully created result json at - ./result/result.json");
+
+        // Writing output Json results...
+        out.println("Writing output Json results...");
+        mainObj.writeJsonToFile(mainObj.createCategorisedMoviesList(movieMap));
+        out.println("Successfully created result json at - ./result/result.json");
 
     }
 
     private void writeJsonToFile(List<FskCategory> fskCategoryList) {
         try (FileWriter file = new FileWriter("./result/result.json")) {
-
             file.write(new Gson().toJson(fskCategoryList));
             file.flush();
-
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private List<FskCategory> createCategorisedMoviesList(Map<String, ArrayList<Movie>> movieMap) {
-        List<FskCategory> fskCategoryList = new ArrayList<>();
-        movieMap.forEach((k,v) -> {
-            fskCategoryList.add(new FskCategory(k, movieMap.get(k).size(), movieMap.get(k)));
-        });
-        return fskCategoryList;
+    private List<FskCategory> createCategorisedMoviesList(@NotNull Map<String, ArrayList<Movie>> movieMap) {
+        return movieMap.keySet().stream().map(k -> new FskCategory(k, movieMap.get(k).size(), movieMap.get(k))).collect(Collectors.toList());
     }
 
-    private void createMoviesOutputJson(Map<String, ArrayList<Movie>> movieMap) throws IOException {
-        Gson gson = new Gson();
-        JsonWriter writer = new JsonWriter(new BufferedWriter(new FileWriter(new File("./result/result.json"))));
-        writer.beginArray();
-        movieMap.forEach((k,v) -> {
-            JsonElement element = gson.toJsonTree(new FskCategory(k, movieMap.get(k).size(), movieMap.get(k)), FskCategory.class);
-            try {
-                writer.jsonValue(gson.toJson(element));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-
-        writer.endArray();
-        writer.close();
-    }
-
-    private Map<String, ArrayList<Movie>> groupMoviesByCountAndFSKType(List<Movies> moviesList) {
+    private Map<String, ArrayList<Movie>> groupMoviesByCountAndFSKType(@NotNull List<Movies> moviesList) {
         Map<String, ArrayList<Movie>> movieMap = new HashMap<>();
         moviesList.forEach(m -> {
             String fskArray = String.join(",", m.getFsk_level_list_facet());
             movieMap.computeIfAbsent(fskArray, k -> new ArrayList<>());
-            movieMap.get(fskArray).add(new Movie(m.getId(),
-                    m.getTitle(),
-                    m.getProduction_year()));
+            movieMap.get(fskArray).add(new Movie(m.getId(), m.getTitle(), m.getProduction_year()));
         });
         return movieMap;
     }
 
     private List<Movies> extractMoviesFromJson(File inputJsonFile) throws FileNotFoundException {
-        Gson gson = new Gson();
-        List<Movies> allAssets = gson.fromJson(new BufferedReader( new FileReader(inputJsonFile)), new TypeToken<List<Movies>>(){}.getType());
+        List<Movies> allAssets = new Gson().fromJson(new BufferedReader( new FileReader(inputJsonFile)), new TypeToken<List<Movies>>(){}.getType());
         return allAssets.stream().filter(m -> m.getObject_class().equalsIgnoreCase("Movie")).collect(Collectors.toList());
     }
 
+    @NotNull
     private File loadInputFile() {
         String fileName = "assets.json";
         ClassLoader classLoader = Main.class.getClassLoader();
